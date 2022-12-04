ef28dae357f50d8df4a377d9004d8d87c59feed4
Pavel Los <los.pavel.21@gmail.com>
2022-12-04 22:04:03 +0300

    feat(app): add MinValueValidator for ServiceArea price

--- app/polygon/models/service_area.py
+++ app/polygon/models/service_area.py
@@ -1,11 +1,16 @@
+from decimal import Decimal
+
 from django.contrib.gis.db import models
+from django.core.validators import MinValueValidator
 
 from .base import BaseModel
 
 
 class ServiceArea(BaseModel):
     name = models.CharField(max_length=256, null=False)
-    price = models.DecimalField(max_digits=10, decimal_places=3, null=False)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=3, null=False, validators=[MinValueValidator(Decimal("0.000"))]
+    )
     geo_info = models.MultiPolygonField(null=False)
 
     provider = models.ForeignKey(
