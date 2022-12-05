bc9bd88df6fc3933b14aea9f1a4769ab07b6e34d
Pavel Los <Pavel.Los@itechart-group.com>
2022-12-05 20:00:28 +0300

    fix(linter): fix warnings

--- app/manage.py
+++ app/manage.py
@@ -8,8 +8,8 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
     try:
-        from django.core.management import (
-            execute_from_command_line,  # pylint: disable=import-outside-toplevel
+        from django.core.management import (  # pylint: disable=import-outside-toplevel
+            execute_from_command_line,
         )
     except ImportError as exc:
         raise ImportError(
